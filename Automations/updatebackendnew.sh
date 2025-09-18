12e056d3e9696b9d719e6d156bf4427f36f20437
sahu-ch-kalu <148070674+sahukalu@users.noreply.github.com>
2025-09-18 16:17:14 +0530

    Update updatebackendnew.sh

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
+# Instance details (from GCP console)
+INSTANCE_NAME="wanderlust-mega-project"
+ZONE="us-central1-f"
 
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Retrieve the public IP address of the specified GCP instance
+ipv4_address=$(gcloud compute instances describe $INSTANCE_NAME \
+  --zone=$ZONE \
+  --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 
 # Path to the .env file
 file_to_find="../backend/.env.docker"
@@ -16,7 +19,10 @@ current_url=$(sed -n "4p" $file_to_find)
 if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
     if [ -f $file_to_find ]; then
         sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
+        echo "Updated FRONTEND_URL to http://${ipv4_address}:5173"
     else
-        echo "ERROR: File not found."
+        echo "ERROR: File not found at $file_to_find"
     fi
+else
+    echo "No change in IP. FRONTEND_URL already set correctly."
 fi
